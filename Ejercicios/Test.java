61c12123cf0f4640f3cd775e69a79a497be06949
GabrielOrcoapaza <128951617+GabrielOrcoapaza@users.noreply.github.com>
2023-05-20 19:51:46 -0500

    Add files via upload

--- Ejercicios/Test.java
+++ Ejercicios/Test.java
@@ -10,9 +10,62 @@ package practica8;
  */
 public class Test {
     public static void main(String[] args) throws ItemDuplicated {
+        BSTree<Integer> tree = new BSTree<>();
+
+        try {
+            tree.insert(5);
+            tree.insert(3);
+            tree.insert(7);
+            tree.insert(2);
+            tree.insert(4);
+            tree.insert(6);
+            tree.insert(8);
+        } catch (ItemDuplicated e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            int searchResult = tree.search(4);
+            System.out.println("Search result: " + searchResult);
+        } catch (ItemNotFound e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            tree.remove(7);
+        } catch (ItemNotFound e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Tree contents: " + tree.toString());
+
+        System.out.println("In-order traversal:");
+        tree.inOrdeb();
+
+        int nonLeafNodes = tree.countNonLeafNodes();
+        System.out.println("Number of non-leaf nodes: " + nonLeafNodes);
+
+        int nodeHeight = tree.getNodeHeight(4);
+        System.out.println("Height of node 4: " + nodeHeight);
+
+        System.out.println("Iterative pre-order traversal:");
+        tree.iterativePreOrder();
+
+        int area = tree.calculateArea();
+        System.out.println("Area of the tree: " + area);
+
+        BSTree<Integer>.Node minNode = tree.findMinNode();
+        System.out.println("Minimum node: " + minNode.data);
+
+        BSTree<Integer>.Node maxNode = tree.findMaxNode();
+        System.out.println("Maximum node: " + maxNode.data);
+
+        System.out.println("Parenthesized tree representation:");
+        tree.parenthesize();
+    
         //Prueba de la parte de ejercicio 5
         // Ingreso de datos para probar el area de los arboles
-        BSTree<Integer> tree1 = new BSTree<>();
+        /*BSTree<Integer> tree1 = new BSTree<>();
         tree1.insert(5);
         tree1.insert(3);
         tree1.insert(8);
@@ -27,7 +80,7 @@ public class Test {
         tree2.insert(6);
 
         boolean sameArea = haveSameArea(tree1, tree2);
-        System.out.println("Los árboles tienen la misma área: " + sameArea); 
+        System.out.println("Los árboles tienen la misma área: " + sameArea);*/ 
         
         // Prueba de la parte de la actividad 3
         /*
@@ -91,12 +144,12 @@ public class Test {
             System.out.println("Error: " + e.getMessage());
         }*/
     }
-        private static boolean haveSameArea(BSTree<Integer> tree1, BSTree<Integer> tree2) {
+        /*private static boolean haveSameArea(BSTree<Integer> tree1, BSTree<Integer> tree2) {
             int area1 = tree1.calculateArea();
             int area2 = tree2.calculateArea();
 
             return area1 == area2;
-        }
+        }*/
         // Prueba con BST de enteros
      
     
